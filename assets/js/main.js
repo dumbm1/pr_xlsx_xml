c155521c79823cb88b946658a7b4f3213b2ac3d5
Marat Shagiev <21020173+dumbm1@users.noreply.github.com>
2024-09-08 02:24:47 +0300

    Make data loading from xlsx to Illustrator stamp

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,13 +3,19 @@
  * Ограничения по габаритам панели заданы в файле CSXS/manifest.xml
  * */
 
-try { fitPanelToContent();} catch (e) { }
+try {
+ fitPanelToContent();
+} catch (e) {
+}
 
 /**
  * Скрипт следит за изменением цветовой схемы Иллюстратора
  * Описание скрипта в файле themeChanger.js
  * */
-try { changeTheme(csInterface);} catch (e) { }
+try {
+ changeTheme(csInterface);
+} catch (e) {
+}
 
 const reload_panel = document.querySelector('#reload_panel');
 reload_panel.addEventListener('click', () => {
@@ -18,28 +24,11 @@ reload_panel.addEventListener('click', () => {
 
 loadJSX('json2.js');
 
-getInkCoverage();
-getXlsx();
-setXlsxData();
-
-/*function setXlsxData() {
- const testBtn = document.querySelector('#test');
- testBtn.addEventListener('click', ()=>{
- csInterface.evalScript('setXlsxData(' +JSON.stringify(o) + ')' , function (result) {
- alert(result);
- });
- })
-
- }*/
-
-function setXlsxData() {
- const testBtn = document.querySelector('#test');
- testBtn.addEventListener('click', () => {
-
-  csInterface.evalScript('setXlsxData(' + JSON.stringify(getXlsx()) + ')', function (result) {
-  });
- });
-
+try {
+ getInkCoverage();
+ getXlsx();
+} catch (e) {
+ alert(e);
 }
 
 function getXlsx() {
@@ -51,136 +40,12 @@ function getXlsx() {
 
   try {
    return getFf(e);
-   /*   const file = e.target.files[0];
-    const data = await file.arrayBuffer();
-
-    const workBook = XLSX.read(data);
-    const workSheet = workBook.Sheets[workBook.SheetNames[0]];
-
-    const dataForTable = {
-    customerCompanyName: workSheet.C4?.v,
-    orderNumber: workSheet.C5?.v,
-    orderName: workSheet.E6?.v,
-
-    printSideUp: workSheet.F23?.v,
-    printSideDn: workSheet.F24?.v,
-
-    formCilinder: workSheet.E40?.v,
-    rapport: workSheet.L42?.v,
-    streamWidth: workSheet.L40?.v,
-    streamsNumber: workSheet.D6?.v,
-    supports: workSheet.L44?.v,
-    filmWidth: workSheet.E42?.v,
-
-    inkNumber: workSheet.E43?.v,
-    inkChange: workSheet.L37?.v,
-
-    sensorLabelSize: workSheet.J49?.v,
-    sensorLabelColor: workSheet.J51?.v,
-    sensorFieldColor: workSheet.J50?.v,
-
-    filmPrint: workSheet.F53?.v,
-    filmCover: workSheet.F54?.v,
-
-    windingSchema: workSheet.L46?.v,
-
-    getFilmComposition() {
-    try {
-    if (this.filmPrint && this.filmCover) {
-    return this.filmPrint + '+' + this.filmCover;
-    } else if (this.filmPrint) {
-    return this.filmPrint;
-    } else {
-    return new Error('Уточнить состав готового материала');
-    }
-    } catch (e) {
-    return new Error('Неизвестная ошибка определения состава материала');
-    }
-    },
-
-    getPrintSide() {
-    try {
-    if (this.printSideUp) {
-    return 'Прямая';
-    } else if (this.printSideDn) {
-    return 'Обратная';
-    } else {
-    return new Error('уточнить тип печати');
-    }
-    } catch (e) {
-    return new Error('Неизвестная ошибка определения типа печати');
-    }
-    },
-
-    getMountWidth() {
-    try {
-    const MIN_PRINT_FIELDS = 4;
-    const MIN_SUPPORTS_CUTTING_FIELDS = 4;
-    const MIN_SUPPORTS_WIDTH = 4;
-    const filmW = this.filmWidth;
-    const printW = +this.streamWidth * +this.streamsNumber;
-    const totalFields = filmW - printW - MIN_PRINT_FIELDS;
-    if (!this.supports.match(/кресты/i)) return filmW;
-    return filmW + 14;
-    } catch (e) {
-    return new Error('Неизвестная ошибка определения ширины монтажа');
-    }
-
-    }
-    };
-
-    Object.defineProperty(dataForTable, 'getPrintSide', {enumerable: false});
-    Object.defineProperty(dataForTable, 'getFilmComposition', {enumerable: false});
-    Object.defineProperty(dataForTable, 'getMountWidth', {enumerable: false});
-
-    for (let key in dataForTable) {
-    let val = dataForTable[key];
-    outStr += val + ';\n';
-    }
-
-    output.value = outStr +
-    dataForTable.getPrintSide() + '\n' + dataForTable.getFilmComposition() + '\n' + dataForTable.getMountWidth();
-
-    return dataForTable;*/
-
-
-
   } catch (e) {
-   output.innerHTML = e;
-   return;
+   return new Error('Error in getXlsx' + e.message);
   }
 
  });
- return 'Вот блять тебе и о!';
 }
-
-`Номер C5 
-Наименование продукта E6
-Заказчик C4
-
-тип печати
-F23 - прямая
-F24 - обратная
-
-формный вал E40
-Раппорт L42
-Ширина ручья L40
-Количество ручьев D6
-Приводные элементы (если есть "кресты", то ширина монтажа + 14 или + 16) L44
-Ширина материала (в таблице не надо) E42
-
-Кол-во красок E43
-Меняется красок L37
-
-Размер метки J49
-Цвет поля метки J50
-Цвет метки J51
-
-Материал запечатываемый F53
-Матриал покрывной F54
-
-Схема намотки L46
-`;
 
 function getInkCoverage() {
  const inkCoverageBtn = document.querySelector('#ink_coverage');
@@ -211,12 +76,14 @@ function getInkCoverage() {
     output.value += ' '; // костыль
     output.value = output.value.slice(0, -1); // костыль
    });
-  } catch (e) {alert(e); }
+  } catch (e) {
+   return ('Error in getInkCoverage(). ' + e);
+  }
  });
 }
 
 function loadJSX(fileName) {
- var extensionRoot = csInterface.getSystemPath(SystemPath.EXTENSION) + '/assets/jsx/';
+ const extensionRoot = csInterface.getSystemPath(SystemPath.EXTENSION) + '/assets/jsx/';
  csInterface.evalScript('$.evalFile("' + extensionRoot + fileName + '")');
 }
 
@@ -251,75 +118,24 @@ async function getFf(e) {
 
   sensorLabelSize: workSheet.J49?.v,
   sensorLabelColor: workSheet.J51?.v,
-  sensorFieldColor: workSheet.J50?.v,
+  sensorLabelFieldColor: workSheet.J50?.v,
 
   filmPrint: workSheet.F53?.v,
   filmCover: workSheet.F54?.v,
 
   windingSchema: workSheet.L46?.v,
-
-  filmComposition: '-', //this.getFilmComposition(),
-  printSide: '-', // this.getPrintSide(),
-
-  getFilmComposition() {
-   try {
-    if (this.filmPrint && this.filmCover) {
-     return this.filmPrint + '+' + this.filmCover;
-    } else if (this.filmPrint) {
-     return this.filmPrint;
-    } else {
-     return new Error('Уточнить состав готового материала');
-    }
-   } catch (e) {
-    return new Error('Неизвестная ошибка определения состава материала');
-   }
-  },
-
-  getPrintSide() {
-   try {
-    if (this.printSideUp) {
-     return 'Прямая';
-    } else if (this.printSideDn) {
-     return 'Обратная';
-    } else {
-     return new Error('уточнить тип печати');
-    }
-   } catch (e) {
-    return new Error('Неизвестная ошибка определения типа печати');
-   }
-  },
-
-  /*getMountWidth() {
-   try {
-   const MIN_PRINT_FIELDS = 4;
-   const MIN_SUPPORTS_CUTTING_FIELDS = 4;
-   const MIN_SUPPORTS_WIDTH = 4;
-   const filmW = this.filmWidth;
-   const printW = +this.streamWidth * +this.streamsNumber;
-   const totalFields = filmW - printW - MIN_PRINT_FIELDS;
-   if (!this.supports.match(/кресты/i)) return filmW;
-   return filmW + 14;
-   } catch (e) {
-   return new Error('Неизвестная ошибка определения ширины монтажа');
-   }
-
-   }*/
  };
-
- Object.defineProperty(dataForTable, 'getPrintSide', {enumerable: false});
- Object.defineProperty(dataForTable, 'getFilmComposition', {enumerable: false});
- // Object.defineProperty(dataForTable, 'getMountWidth', {enumerable: false});
 
  for (let key in dataForTable) {
   let val = dataForTable[key];
-  outStr += val + ';\n';
+  outStr += key + ': ' + val + '(' + typeof val + ');\n';
  }
 
  output.value = outStr /*+
   dataForTable.getPrintSide() + '\n' + dataForTable.getFilmComposition() + '\n' + dataForTable.getMountWidth()*/;
 
  csInterface.evalScript('setXlsxData(' + JSON.stringify(dataForTable) + ')', function (result) {
-  alert(result);
+  outStr += '\n\n' + 'jsx setXlsxData resutlt is: ' + result;
  });
 
  return dataForTable;
--- assets/jsx/hostscript.jsx
+++ assets/jsx/hostscript.jsx
@@ -1,5 +1,3 @@
-//@include json2.js
-
 function getXmlStr() {
 
  if (!documents.length) {
@@ -26,92 +24,80 @@ function getXmlStr() {
 }
 
 function setXlsxData(o) {
- var prStampNames = [
-  '__pr-stamp__orderNumber__',
-  '__pr-stamp__customerCompanyName__',
-  '__pr-stamp__orderName__',
+ var ad = activeDocument;
+
+ var stampFieldNames = [
+  '__pr-stamp__orderNumber_mainTable__',
+  '__pr-stamp__customerCompanyName_mainTable__',
+  '__pr-stamp__orderName_mainTable__',
+
+  '__pr-stamp__orderNumber_txtTable__',
+  '__pr-stamp__customerCompanyName_txtTable__',
+  '__pr-stamp__orderName_txtTable__',
+
   '__pr-stamp__sensorLabelSize__',
   '__pr-stamp__sensorLabelColor__',
   '__pr-stamp__sensorLabelFieldColor__',
+
+  '__pr-stamp__inkNumber__',
+  '__pr-stamp__inkChange__',
+
   '__pr-stamp__icmProfile__',
   '__pr-stamp__printSide__',
   '__pr-stamp__filmComposition__',
-  '__pr-stamp__incNumber__',
-  '__pr-stamp__incChange__',
   '__pr-stamp__windingSchema__'
  ];
 
- function push2obj(obj) {
-  var errors = '';
+ return push2obj(stampFieldNames, o);
+
+ function push2obj(stampFieldNames, o) {
+  var errors = 'Host error-list:\n\n';
   var ad = activeDocument;
-  for (var i = 0; i < prStampNames.length; i++) {
-   var currStampName = prStampNames[i];
-   try {
-    var currStampElem = ad.TextFrames.getByName(currStampName);
-    currStampElem.contents = o[currStampName.slice(12, -2)];
-   } catch (e) {
-    errors += '\n';
+
+  for (var i = 0; i <= stampFieldNames.length; i++) {
+   var fieldName = stampFieldNames[i];
+
+   if (fieldName.match('_mainTable__')) {
+    try {
+     var fieldElem = ad.textFrames.getByName(fieldName);
+     fieldElem.contents = o[fieldName.slice(12, -12)];
+    } catch (e) {
+     errors += e.line + '. ' + e.message + '\n';
+    }
+   } else if (fieldName.match('_txtTable__')) {
+    try {
+     var fieldElem = ad.textFrames.getByName(fieldName);
+     fieldElem.contents = o[fieldName.slice(12, -11)];
+    } catch (e) {
+     errors += e.line + '. ' + e.message + '\n';
+    }
+   } else {
+    try {
+     var fieldElem = ad.textFrames.getByName(fieldName);
+
+     switch (fieldName) {
+      case '__pr-stamp__printSide__':
+       if (o.printSideUp) {
+        fieldElem.contents = 'Прямая бля!!!';
+       } else if (o.printSideDn) {
+        fieldElem.contents = 'Ебать, Обратная!!!';
+       }
+       break;
+      case '__pr-stamp__filmComposition__':
+       fieldElem.contents = o.filmPrint + '+' + o.filmCover || '';
+       break;
+      default:
+       fieldElem.contents = o[fieldName.slice(12, -2)] || '—';
+       break;
+     }
+
+    } catch (e) {
+     errors += e.line + '. ' + e.message + '\n';
+    }
    }
+
   }
-  alert(errors);
+  // alert(errors);
+  return errors;
  }
 }
-
-`
-  customerCompanyName: workSheet.C4?.v,
-  orderNumber: workSheet.C5?.v,
-  orderName: workSheet.E6?.v,
-
-  printSideUp: workSheet.F23?.v,
-  printSideDn: workSheet.F24?.v,
-
-  formCilinder: workSheet.E40?.v,
-  rapport: workSheet.L42?.v,
-  streamWidth: workSheet.L40?.v,
-  streamsNumber: workSheet.D6?.v,
-  supports: workSheet.L44?.v,
-  filmWidth: workSheet.E42?.v,
-
-  inkNumber: workSheet.E43?.v,
-  inkChange: workSheet.L37?.v,
-
-  sensorLabelSize: workSheet.J49?.v,
-  sensorLabelColor: workSheet.J51?.v,
-  sensorFieldColor: workSheet.J50?.v,
-
-  filmPrint: workSheet.F53?.v,
-  filmCover: workSheet.F54?.v,
-
-  windingSchema: workSheet.L46?.v,
-
-  getFilmComposition() {
-   try {
-    if (this.filmPrint && this.filmCover) {
-     return this.filmPrint + '+' + this.filmCover;
-    } else if (this.filmPrint) {
-     return this.filmPrint;
-    } else {
-     return new Error('Уточнить состав готового материала');
-    }
-   } catch (e) {
-    return new Error('Неизвестная ошибка определения состава материала');
-   }
-  },
-
-  getPrintSide() {
-   try {
-    if (this.printSideUp) {
-     return 'Прямая';
-    } else if (this.printSideDn) {
-     return 'Обратная';
-    } else {
-     return new Error('уточнить тип печати');
-    }
-   } catch (e) {
-    return new Error('Неизвестная ошибка определения типа печати');
-   }
-  },
-`;
-
-
-
